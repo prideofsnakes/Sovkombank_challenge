9afb29c80826c941988ca4a29f8f0438d24543b7
prideofsnakes <vasekbs_91@mail.ru>
2021-05-31 13:30:01 +0300

    Trying to separate db and soap by package 2

---
+++ wsdlWS/src/main/java/h2db/H2Repository.java
@@ -0,0 +1,12 @@
+package h2db;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public interface H2Repository extends CrudRepository<H2User, Long> {
+	
+	H2User findById(long id);
+}
